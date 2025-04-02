8e347e806dfb3e7bbe1294d46453eea263fb7f2e
“Aamir <aamirhussainazad786@gmail.com>
2025-04-02 15:15:56 +0530

    Component Structure

--- frontend/test.js
+++
@@ -1,9 +0,0 @@
-// let n = "Hello, World!";
-// alert(n);
-
-// console.log(n);
-
-
-const prompt = require("prompt-sync")();
-let n = "Hello, World!";
-prompt(n); // This will show the message and wait for user input
