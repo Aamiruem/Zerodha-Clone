1ef5a7b0ca8a3448e4941ea4ebe8eb4da6bf8ecc
“Aamir <aamirhussainazad786@gmail.com>
2025-04-02 13:53:55 +0530

    commit add

---
+++ frontend/test.js
@@ -0,0 +1,9 @@
+// let n = "Hello, World!";
+// alert(n);
+
+// console.log(n);
+
+
+const prompt = require("prompt-sync")();
+let n = "Hello, World!";
+prompt(n); // This will show the message and wait for user input
